62add7dba2e2deaba6cae88c2e5429be21756f99
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-05-15 03:16:35 +0500

    design the card with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
 import './App.css';
+import { BrowserRouter as Router,
+  Routes,
+  Route 
+} from 'react-router-dom';
 
 import Home from './screens/Home';
+import Login from './screens/Login';
+
 
 function App() {
   return (
+    <Router>
     <div>
-     <Home/>
-      
+      <Routes>
+        <Route exact path = "/" element= {<Home/>}> </Route>
+        <Route exact path = "login" element= {<Login/>}> </Route>
+      </Routes>
+    
     </div>
+    </Router>
+    
   );
 }
 
